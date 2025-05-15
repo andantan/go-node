f4ae4ec9df2841503205bc1e58b87127c83efe4a
andantan <kyubin2892@gmail.com>
2025-05-16 01:52:18 +0900

    Fixed elliptic gob marshalling

--- crypto/keypair.go
+++ crypto/keypair.go
@@ -44,23 +44,27 @@ func GeneratePrivateKey() PrivateKey {
 }
 
 func (k PrivateKey) PublicKey() PublicKey {
-	return PublicKey{
-		Key: &k.key.PublicKey,
-	}
+	// return PublicKey{
+	// 	Key: &k.key.PublicKey,
+	// }
+	return elliptic.MarshalCompressed(k.key.PublicKey, k.key.PublicKey.X, k.key.PublicKey.Y)
 }
+
+type PublicKey []byte
 
 // Wrapped ECDSA
-type PublicKey struct {
-	Key *ecdsa.PublicKey
-}
+// type PublicKey struct {
+// 	Key *ecdsa.PublicKey
+// }
 
-func (k PublicKey) ToSlice() []byte {
-	return elliptic.MarshalCompressed(k.Key, k.Key.X, k.Key.Y)
-}
+// func (k PublicKey) ToSlice() []byte {
+// 	return elliptic.MarshalCompressed(k.Key, k.Key.X, k.Key.Y)
+// }
 
 // SHA256 andthen get 20bytes -> Like ethereum
 func (k PublicKey) Address() types.Address {
-	h := sha256.Sum256(k.ToSlice())
+	// h := sha256.Sum256(k.ToSlice())
+	h := sha256.Sum256(k)
 
 	// Create address from given bytes
 	// TODO: Make this readable
@@ -74,5 +78,13 @@ type Signature struct {
 }
 
 func (sig Signature) Verify(pubKey PublicKey, data []byte) bool {
-	return ecdsa.Verify(pubKey.Key, data, sig.R, sig.S)
+	// return ecdsa.Verify(pubKey.Key, data, sig.R, sig.S)
+	x, y := elliptic.UnmarshalCompressed(elliptic.P256(), pubKey)
+	key := &ecdsa.PublicKey{
+		Curve: elliptic.P256(),
+		X:     x,
+		Y:     y,
+	}
+
+	return ecdsa.Verify(key, data, sig.R, sig.S)
 }
